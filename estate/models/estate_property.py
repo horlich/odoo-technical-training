1f81392c5815fff18221bd7dea860615d010f9c1
Herbert Orlich <herbert@orlich.at>
2023-07-30 20:36:45 +0200

    Chapter 7 until Form

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,16 +1,18 @@
 from odoo import fields, models
+import datetime
+from datetime import timedelta
 
 class PropertyModel(models.Model):
     _name = "estate.property"
     _description = "Modul intern"
 
-    name = fields.Char(required=True)
+    name = fields.Char(required=True, string='Title')
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(copy=False, default=lambda self: (fields.Date.today() + datetime.timedelta(90)))
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
@@ -19,5 +21,10 @@ class PropertyModel(models.Model):
     garden_orientation = fields.Selection(
         selection = [('n', "North"), ('s', "South"), ('e', "East"), ('w', "West")]
     )
+    state = fields.Selection(
+        selection = [('n', "New"), ('r', "Offer Received"), ('a', "Offer Accepted"), ('s', "Sold"), ('c', "Cancelled")],
+        default = 'n'
+    )
+    active = fields.Boolean(default=True)
 
     
